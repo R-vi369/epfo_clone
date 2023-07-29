04ddd4b1b8f5aba63707e25f8c94c610775967bf
ravi <sama369.rk@gmail.com>
2023-07-29 10:48:06 +0530

    working on nav bar

--- epfo_clone/src/component/Hero.jsx
+++ epfo_clone/src/component/Hero.jsx
@@ -1,32 +1,115 @@
 import React from "react";
 import "../Style/Hero.scss";
+import ArrowDropDownRoundedIcon from "@mui/icons-material/ArrowDropDownRounded";
+
+import ArrowDropUpRoundedIcon from "@mui/icons-material/ArrowDropUpRounded";
+import Navbar from './Navbar'
 const Hero = () => {
   return (
     <>
+      {/* <p>pliance of the direction contained in the para 44 (iii) & (iv) read with Para 44(v) of the judgement (for employees who were in service prior to 01.09.2014 and continued to be in service on or after 01.09.2014 but could not exercise joint option under erstwhile proviso to para 11(3) of EPS-1995), online facility for filing joint option is available at unified portal member interface</p> */}
       <div className="hero">
-        <div className="services">
-          <button className="dropbtn">Services</button>
-          <div className="dropdown_content">
-            <a href="">For Employers</a>
-            <a href="">For Employees</a>
-            <a href="">For International Workers</a>
-            <a href="">For Pensioners - Locate Jeevan Pramaan Center</a>
-            <a href="">Locate an EPFO office</a>
-            <a href="">Who's Who</a>
-          </div>
+        <div className="blank">
+          <a href="" style={{ color: " #27408b", height: "42px" }}>
+            !
+          </a>
         </div>
-        <div className="Exempted_Estt">
-          <button>Exempted Estt</button>
-          <div className="dropdown_content">
-            <a href="">For Employers</a>
-            <a href="">For Employees</a>
-            <a href="">For International Workers</a>
-            <a href="">For Pensioners - Locate Jeevan Pramaan Center</a>
-            <a href="">Locate an EPFO office</a>
-            <a href="">Who's Who</a>
+        <div className="blank">
+          <a href="">
+            <ArrowDropUpRoundedIcon />
+          </a>
+        </div>
+        <div className="services">
+          <a href="">
+            Services
+            <ArrowDropDownRoundedIcon />
+          </a>
+        </div>
+        <div className="exempted_Estt">
+          <a href="">
+            Exempted Estt
+            <ArrowDropDownRoundedIcon />
+          </a>
+        </div>
+
+        <div className="epfo_Corner">
+          <a href="">
+            EPFO Corner
+            <ArrowDropDownRoundedIcon />
+          </a>
+        </div>
+        <div className="miscellaneous">
+          <a href="">
+            Miscellaneous
+            <ArrowDropDownRoundedIcon />
+          </a>
+        </div>
+
+        <div className="Directory">
+          <a href="">Directory</a>
+        </div>
+        <div className="abry">
+          <a href="">ABRY</a>
+        </div>
+        <div className="Dashboards">
+          <a href="">Dashboards</a>
+        </div>
+        <div className="Covid-19">
+          <a href="">Covid-19</a>
+        </div>
+        <div className="Downloads">
+          <a href="">
+            Downloads
+            <ArrowDropDownRoundedIcon />
+          </a>
+        </div>
+        <div className="blank">
+          <a href="" style={{ color: " #27408b", height: "42px" }}>
+            !
+          </a>
+        </div>
+      </div>
+
+      <div className="stay_box">
+        <div className="stay_alert">
+          <img
+            src="https://www.epfindia.gov.in/images/Fraud_Alert_en.jpeg"
+            alt=""
+          />
+        </div>
+
+        <div className="box_8">
+          <div className="box_1">
+            <button>Establishment</button>
+            <button> KYC Updation Member</button>
+          </div>
+          <div className="box_2">
+            <button>UMANG</button>
+            <button>ECR/Retu?Paymenrnst</button>
+          </div>
+          <div className="box_3">
+            <button>
+              {" "}
+              Online Claims <br /> Member Account Transfer
+            </button>
+            <button> e-Passbook</button>
+          </div>
+
+          <div className="box_4">
+            <button>Performance of Establishements</button>
+            <button>
+              {" "}
+              Shram Suvidha <br />
+              Common ECR EPFO/ESIC
+            </button>
           </div>
         </div>
       </div>
+
+
+
+
+       <Navbar/>
     </>
   );
 };
---
+++ epfo_clone/src/component/Navbar.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import WhatsAppIcon from "@mui/icons-material/WhatsApp";
+import '../Style/Navbar.scss'
+const Navbar = () => {
+  return (
+    <>
+      <div className="nav">
+        <div className="icons">
+          <WhatsAppIcon />
+          <WhatsAppIcon />
+          <WhatsAppIcon />
+          <WhatsAppIcon />
+          <WhatsAppIcon />
+          <WhatsAppIcon />
+          <WhatsAppIcon />
+          <WhatsAppIcon />
+          <WhatsAppIcon />
+          <WhatsAppIcon />
+          <WhatsAppIcon />
+          <WhatsAppIcon />
+          <WhatsAppIcon />
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Navbar;
