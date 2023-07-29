5c8fc69ced1e7c70179e104b1e77922a20c4f923
ravi <sama369.rk@gmail.com>
2023-07-29 11:53:58 +0530

    nav completed

--- epfo_clone/src/component/Navbar.jsx
+++ epfo_clone/src/component/Navbar.jsx
@@ -1,24 +1,62 @@
-import React from "react";
-import WhatsAppIcon from "@mui/icons-material/WhatsApp";
+
+import help from '../assets/help.png'
+import faq from '../assets/faq.png'
+import whatApp from '../assets/whatApp.png'
+import facebook from '../assets/facebook.png'
+import twitter from '../assets/twitter.png'
+import youtube from '../assets/youtube.png'
+import phone from '../assets/phone.png'
+import grievence from '../assets/grivence.png'
+import digilocker from '../assets/digilocker.png'
+import umang from '../assets/Umang.png'
+import insta from '../assets/insta.png'
+import bird from '../assets/bird.png'
+import public1 from '../assets/public1.png'
 import '../Style/Navbar.scss'
 const Navbar = () => {
   return (
     <>
       <div className="nav">
         <div className="icons">
-          <WhatsAppIcon />
-          <WhatsAppIcon />
-          <WhatsAppIcon />
-          <WhatsAppIcon />
-          <WhatsAppIcon />
-          <WhatsAppIcon />
-          <WhatsAppIcon />
-          <WhatsAppIcon />
-          <WhatsAppIcon />
-          <WhatsAppIcon />
-          <WhatsAppIcon />
-          <WhatsAppIcon />
-          <WhatsAppIcon />
+          <div className="help">
+            <img src={help} alt="" />
+          </div>
+          <div className="faq">
+            <img src={faq} alt="" />
+          </div> 
+          <div className="whatApp">
+            <img src={whatApp} alt="" />
+          </div>
+          <div className="facebook">
+            <img src={facebook} alt="" />
+          </div>
+          <div className="twitter">
+            <img src={twitter} alt="" />
+          </div>
+          <div className="youtube">
+            <img src={youtube} alt="" />
+          </div>
+          <div className="phone">
+            <img src={phone} alt="" />
+          </div>
+          <div className="grievence">
+            <img src={grievence} alt="" />
+          </div>
+          <div className="digi">
+            <img src={digilocker} alt="" />
+          </div>
+          <div className="umang">
+            <img src={umang} alt="" />
+          </div>
+          <div className="insta">
+            <img src={insta} alt="" />
+          </div>
+          <div className="bird">
+            <img src={bird} alt="" />
+          </div>
+          <div className="public">
+            <img src={public1} alt="" />
+          </div>
         </div>
       </div>
     </>
