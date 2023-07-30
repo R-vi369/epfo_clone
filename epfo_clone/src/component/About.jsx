4fed8b1c47384da8cee3af401524889fd5a003d6
ravi <sama369.rk@gmail.com>
2023-07-30 16:09:53 +0530

    working on about section

---
+++ epfo_clone/src/component/About.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+import "../Style/About.scss";
+const About = () => {
+  return (
+    <>
+      <div className="about">
+        <div className="about_left">
+          <div className="about_left_1">
+            <div className="about_left_1_nav">
+              <h2>@</h2>
+              <h2>Online Services</h2>
+            </div>
+            <div className="about_left_1_text">
+              <p>Principal Employers-CAIU Portal</p>
+              <p>
+                Pensioners' Portal<sup>New</sup>
+              </p>
+              <p>
+                EDLI & Pension Calculator <sup>New</sup>
+              </p>
+              <p>Jeevan Pramaan Through Mobile App </p>
+              <p>TRRN Query Search</p>
+              <p>International Workers Portal</p>
+            </div>
+          </div>
+          <div className="about_left_2">,</div>
+        </div>
+        <div className="about_right">k</div>
+      </div>
+    </>
+  );
+};
+
+export default About;
